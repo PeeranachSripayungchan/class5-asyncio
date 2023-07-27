28c70679cac311b6f2e0299cd3e5d1582250a809
PeeranachSripayungchan <71226323+PeeranachSripayungchan@users.noreply.github.com>
2023-07-27 23:55:01 +0700

    Update 1-2-simple-async-wrong.py

--- 1-2-simple-async-wrong.py
+++ 1-2-simple-async-wrong.py
@@ -1,29 +1,46 @@
-import time
 import asyncio
+import time
 
+async def sleep():
+    print(f'Time: {time.time() - start:.2f}')
+    time.sleep(1)
 
-# We add async to our function to declare it as a async-io function.
-async def main(x):
-    print(f"Starting Task {x}")
-    # Here we use asyncio.sleep as it's non-blocking and allowing the program to check on the other main() functions.
-    await asyncio.sleep(3)
-    print(f"Finished Task {x}")
+async def sum(name, numbers):
+    total = 0
+    for number in numbers:
+        print(f'Task {name}: Computing {total}+{number}')
 
+        await sleep()
+        total += number
+    print(f'Task {name}: Sum = {total}\n')
 
-# Same declaration of async functions
-async def async_io():
-    # Here we create a list that tracks all the async tasks to-do.
-    tasks = []
-    for i in range(10):
-        # Here we add the task of main() function 10 times and add it to the tasks list. Notice that the main is
-        # wrapped in []
-        tasks += [main(i)]
-    # Finally, asyncio gathers the result of all the tasks in the task list. Notice the * asterisk
-    await asyncio.gather(*tasks)
+start = time.time()
 
-if __name__ == "__main__":
-    start_time = time.perf_counter()
-    # This executes the async_io function as a asynchronous function from a non-async function.
-    # Yes the if condition above executes as a non-async function.
-    asyncio.run(async_io())
-    print(f"Took {time.perf_counter() - start_time} secs")
+loop = asyncio.get_event_loop()
+tasks = [
+    loop.create_task(sum("A", [1, 2])),
+    loop.create_task(sum("B", [1, 2, 3])),
+]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+
+end = time.time()
+print(f'Time: {end-start:.2f} sec')
+
+# Task A จะ run ก่อน Task B
+# Result
+# Task A: Computing 0+1
+# Time: 0.00
+# Task A: Computing 1+2
+# Time: 1.01
+# Task A: Sum = 3
+ 
+# Task B: Computing 0+1
+# Time: 2.02
+# Task B: Computing 1+2
+# Time: 3.02
+# Task B: Computing 3+3
+# Time: 4.03
+# Task B: Sum = 6
+
+# Time: 5.04 sec
