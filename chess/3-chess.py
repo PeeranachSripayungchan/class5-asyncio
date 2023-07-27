031981f490a92a48bc0d178f6fbaad0c03d8d18e
PeeranachSripayungchan <71226323+PeeranachSripayungchan@users.noreply.github.com>
2023-07-27 23:57:07 +0700

    Update 3-chess.py

--- chess/3-chess.py
+++ chess/3-chess.py
@@ -25,3 +25,17 @@ if __name__ == "__main__":
     for j in range(opponents):
         main(j)
     print(f"Finished in {round(time.perf_counter() - start_time)} secs")
+
+# The entire exhibition takes == 720 minutes.
+# my use 5 secs
+# opponent use 55 secs
+# 1 move_pairs = 5 + 55 = 60 secs
+# 30 move_pairs = 60 * 30 = 1800 secs = 1 opponent
+# 1 opponents = 1800 secs
+# 24 opponents = 1800*24 = 43200 secs
+# 43200/60 = 720 minutes
+
+# if opponents = 100
+# 1 opponents = 1800 secs
+# 100 opponents = 1800*100 = 180000 secs
+# 180000/60 = 3000 minutes
