45f05f9435ef770926087061a0e451cff7a13584
PeeranachSripayungchan <71226323+PeeranachSripayungchan@users.noreply.github.com>
2023-07-27 23:56:07 +0700

    Update 2-1-basic.py

---
+++ 2-1-basic.py
@@ -0,0 +1,27 @@
+import asyncio
+import time
+
+async def hello(i):
+    print(f"{time.ctime()} hello {i} started")
+    await asyncio.sleep(4)
+    print(f"{time.ctime()} hello {i} done")
+
+async def main():
+    task1 = asyncio.create_task(hello(1))
+    #await asyncio.sleep(3)
+    task2 = asyncio.create_task(hello(2))
+    await task1
+    await task2
+
+if __name__ == '__main__':
+    start = time.time()
+    asyncio.run(main())
+    end = time.time()
+    print(f'Time: {end-start:.2f} sec')
+
+# Result
+# Wed Jul 26 15:01:34 2023 hello 1 started
+# Wed Jul 26 15:01:34 2023 hello 2 started
+# Wed Jul 26 15:01:38 2023 hello 1 done
+# Wed Jul 26 15:01:38 2023 hello 2 done
+# Time: 4.01 sec
